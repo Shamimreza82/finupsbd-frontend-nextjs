e704c2b6646a8510a775bfdcb5932bbd18d73dfb
Shamimreza82 <shamimrezaone@gmail.com>
2025-02-19 23:17:33 +0600

    update navber

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server"
 import { getCurrentUser } from "./services/AuthService"
 
 
-type Role = keyof typeof roleBasedPrivateRoutes
+// type Role = keyof typeof roleBasedPrivateRoutes
 
 const authRoutes = ["/login", "/register"]
 
-const roleBasedPrivateRoutes = {
-    user: [/^\/user/, /^\/create-shop/], 
-    admin: [/^\/admin/]
-}
+// const roleBasedPrivateRoutes = {
+//     user: [/^\/user/, /^\/create-shop/], 
+//     admin: [/^\/admin/]
+// }
 
 export const middleware = async (request: NextRequest) => {
     const { pathname } = request.nextUrl
@@ -25,13 +25,13 @@ export const middleware = async (request: NextRequest) => {
 
     } 
 
-    if(userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]){
-            const routes = roleBasedPrivateRoutes[userInfo?.role as Role]
-            if(routes.some(route => pathname.match(route))){
-                return NextResponse.next()
-            }
+    // if(userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]){
+    //         const routes = roleBasedPrivateRoutes[userInfo?.role as Role]
+    //         if(routes.some(route => pathname.match(route))){
+    //             return NextResponse.next()
+    //         }
 
-    }
+    // }
 
     return NextResponse.redirect(new URL("/", request.url))
 }
